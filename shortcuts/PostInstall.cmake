189442d4553b9fccdc8a102e2dd2cfaf7dda0517
Carlos Agüero <caguero@osrfoundation.org>
2015-02-17 15:43:46 -0800

    Removing post install scripts.

--- shortcuts/PostInstall.cmake
+++
@@ -1,7 +0,0 @@
-set (post_install_script "PostInstall.sh")
-execute_process(COMMAND ${post_install_script}
-  WORKING_DIRECTORY ${PROJECT_CURRENT_SOURCE_DIR}
-  RESULT_VARIABLE post_install_result
-  OUTPUT_VARIABLE post_install_output)
-
-message(STATUS: "${post_install_output}")
--- shortcuts/PostInstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-chown haptix:haptix /home/haptix/Desktop/haptixStart.desktop
-chown haptix:haptix /home/haptix/Desktop/haptixUpdate.desktop
-chown haptix:haptix /home/haptix/Desktop/haptixSupport.desktop
