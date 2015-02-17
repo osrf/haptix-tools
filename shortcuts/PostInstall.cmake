9bcf19188d8c7d0024f0443219e82a5ea7c2a1d6
Carlos Agüero <caguero@osrfoundation.org>
2015-02-17 14:27:09 -0800

    Adding postinstall script.

---
+++ shortcuts/PostInstall.cmake
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown haptix:haptix /home/osrf/Desktop/haptixStart.desktop
+chown haptix:haptix /home/osrf/Desktop/haptixUpdate.desktop
+chown haptix:haptix /home/osrf/Desktop/haptixSupport.desktop
