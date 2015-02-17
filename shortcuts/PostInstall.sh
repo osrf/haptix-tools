a38acc1906202faa19b965e9dc76ef758f090c78
Carlos Agüero <caguero@osrfoundation.org>
2015-02-17 14:53:35 -0800

    Testing.

---
+++ shortcuts/PostInstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+chown haptix:haptix /home/haptix/Desktop/haptixStart.desktop
+chown haptix:haptix /home/haptix/Desktop/haptixUpdate.desktop
+chown haptix:haptix /home/haptix/Desktop/haptixSupport.desktop
