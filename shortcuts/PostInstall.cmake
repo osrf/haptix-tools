9e9715c459f0ed7dec3435d30c48383e2f6d4070
Carlos Agüero <caguero@osrfoundation.org>
2015-02-17 15:08:38 -0800

    Testing.

--- shortcuts/PostInstall.cmake
+++ shortcuts/PostInstall.cmake
@@ -1,1 +1,7 @@
-execute_process(COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/PostInstall.sh)
+set (post_install_script "PostInstall.sh")
+execute_process(COMMAND ${post_install_script}
+  WORKING_DIRECTORY ${PROJECT_CURRENT_SOURCE_DIR}
+  RESULT_VARIABLE post_install_result
+  OUTPUT_VARIABLE post_install_output)
+
+message(STATUS: "${post_install_output}")
