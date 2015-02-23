261d843a379fe657cf343ebed1d9dd1ac7ea8499
Carlos Aguero <caguero@osrfoundation.org>
2015-02-23 13:17:21 -0800

    Tweaking the script.

--- shortcuts/PreInstall.cmake
+++ shortcuts/PreInstall.cmake
@@ -17,20 +17,26 @@
 ## Author: Open Source Robotics Foundation.
 ## Description: Create the "haptix" user and SSH keys if needed.
 
+NEW_USER=0
+
 # Check if there is a "haptix" user
-getent passwd haptix
+getent passwd haptix > /dev/null
 
 if [ $? -ne 0 ]; then
   # Create the "haptix" user add the user to the "sudo" group.
   useradd -m haptix -G sudo -s /bin/bash
 
-  # Tell the user to create a password for the "haptix" user.
-  echo "Don't forget to create a password for the haptix user:"
-  echo "  sudo passwd haptix"
+  NEW_USER=1
 fi
 
 # Check if the user has a SSH key pair.
 if [ ! -f /home/haptix/.ssh/id_rsa.pub ]; then
   # Generate SSH key.
-  su haptix -c "ssh-keygen -t rsa -N \"\""
+  su haptix -c "ssh-keygen -q -t rsa -N \"\" -f /home/haptix/.ssh/id_rsa"
+fi
+
+if [ $NEW_USER -eq 1 ]; then
+  # Tell the user to create a password for the "haptix" user.
+  echo "Don't forget to create a password for the haptix user:"
+  echo "  sudo passwd haptix"
 fi
