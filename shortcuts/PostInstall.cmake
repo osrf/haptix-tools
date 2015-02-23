b6ed5eecb457521e6ce79cf7e17bbb0505436247
Carlos Agüero <caguero@osrfoundation.org>
2015-02-23 10:21:51 -0800

    Adding checkStereo launcher.

--- shortcuts/PostInstall.cmake
+++ shortcuts/PostInstall.cmake
@@ -4,4 +4,6 @@ EXECUTE_PROCESS(COMMAND chown haptix /home/haptix/Desktop/haptixStart.desktop
                 COMMAND chown haptix /home/haptix/Desktop/haptixUpdate.desktop
                 COMMAND chgrp haptix /home/haptix/Desktop/haptixUpdate.desktop
                 COMMAND chown haptix /home/haptix/Desktop/haptixSupport.desktop
-                COMMAND chgrp haptix /home/haptix/Desktop/haptixSupport.desktop)
+                COMMAND chgrp haptix /home/haptix/Desktop/haptixSupport.desktop
+                COMMAND chown haptix /home/haptix/Desktop/checkStereo.desktop
+                COMMAND chgrp haptix /home/haptix/Desktop/checkStereo.desktop)
