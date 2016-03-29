e29be78fd115893e2143e8e1ab3eb729892b8212
John Hsu <hsu@osrfoundation.org>
2016-03-29 16:29:23 -0700

    add desktop startup icons for luke right and left hands

--- shortcuts/PostInstall.cmake
+++ shortcuts/PostInstall.cmake
@@ -1,6 +1,10 @@
 # Change the owner of the launchers.
-EXECUTE_PROCESS(COMMAND chown haptix /home/haptix/Desktop/haptixStart.desktop
-                COMMAND chgrp haptix /home/haptix/Desktop/haptixStart.desktop
+EXECUTE_PROCESS(COMMAND chown haptix /home/haptix/Desktop/haptixMPLStart.desktop
+                COMMAND chgrp haptix /home/haptix/Desktop/haptixMPLStart.desktop
+                COMMAND chown haptix /home/haptix/Desktop/haptixLukeRightStart.desktop
+                COMMAND chgrp haptix /home/haptix/Desktop/haptixLukeRightStart.desktop
+                COMMAND chown haptix /home/haptix/Desktop/haptixLukeLeftStart.desktop
+                COMMAND chgrp haptix /home/haptix/Desktop/haptixLukeLeftStart.desktop
                 COMMAND chown haptix /home/haptix/Desktop/haptixUpdate.desktop
                 COMMAND chgrp haptix /home/haptix/Desktop/haptixUpdate.desktop
                 COMMAND chown haptix /home/haptix/Desktop/haptixSupport.desktop
